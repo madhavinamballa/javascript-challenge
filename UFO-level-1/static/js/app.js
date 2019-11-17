749c13daeeab1e54ee4602687035671677692cff
Madhavinamballa <madhavi@Madhavinamballas-MacBook-Pro.local>
2019-11-16 23:44:07 -0800

    final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,24 +1,34 @@
-// from data.js
-var country=data;
-console.log(data);
-// YOUR CODE HERE!
-var tbody=d3.select("tbody");
-data.forEach((dataSet) => {
-  var row = tbody.append("tr");
-  Object.entries(dataSet).forEach(([key, value]) => {
-    console.log(key, value);
-    var cell = row.append("td");
-    cell.text(value);
-  });
-});
-  
-  // Function to handle input change
-var button = d3.select("#filter-btn");
+// Variable Declaration
+var tbody = d3.select("tbody");
+var tableData = data;
+var button=d3.select("#filter-btn");
+// Function to generate Table
+function createTable(data){
+    tbody.html("");
+    data.forEach((dataSet) => {
+        var row = tbody.append("tr");
+        Object.entries(dataSet).forEach(([key, value]) => {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+// Function to handle button click events
+function handleClick(){
+    // Prevents the Page from Refreshing
+    d3.event.preventDefault();
+    // Select HTML Input Element & Get the Value Property of that Input Element
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
 
-//input.on("change", handleChange);
-button.on("click",function(){
-  var inputValue=d3.select("#datetime").property("value"); 
-  var filteredData = country.filter(city => city.datetime === inputValue); 
-  console.log(filteredData);
-});
- 
+    // Filter Data Using that Date;
+    if(date) {
+        // Apply Filter to the Table Data to Only Keep Rows Where datetime Value Matches the Filter Value
+        filterData = filterData.filter((row) => row.datetime === date);
+    }
+    // Build Table with Filtered Data
+    createTable(filterData);
+}
+createTable(tableData);
+button.on("click", handleClick);
